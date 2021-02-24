3ad5347b0acf4d3b1a4b5734df413b15e9ef5072
Robert Amanfu <robertamanfu@Roberts-MacBook-Air.local>
2021-02-24 12:03:41 -0800

    created base Jinja template

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/{repo}", response_class=HTMLResponse)
+async def get_repo(request: Request, repo: str):
+    return templates.TemplateResponse("gitviz.html", {"request": request, "repo":repo})
