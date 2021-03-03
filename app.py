fce26c356fea43439a47508fa84d747e831570d0
Robert Amanfu <robertamanfu@Roberts-MacBook-Air.local>
2021-03-02 23:36:40 -0800

    env cleanup

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from utils import get_data, get_dashboard_stats
+import configparser
 
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+config = configparser.ConfigParser()
+config.read('.config')
+access_token = config.get("GITHUB", "token")
+username = 'retroam'
+auth = (username, access_token)
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("welcome.html", {"request": request})
 
 @app.get("/{repo}", response_class=HTMLResponse)
 async def get_repo(request: Request, repo: str):
-    dsh_closed_issues = 4000
+    repo = 'airbnb/knowledge-repo'
+    data = get_data(repo, auth)
+    dsh_open_issues, dsh_closed_issues, dsh_closed_rate,dsh_contrib = get_dashboard_stats(data)
     return templates.TemplateResponse("gitviz.html", {"request": request,
                                                          "repo":repo,
-                                                         "dsh_closed_issues":dsh_closed_issues})
+                                                         "dsh_closed_issues":dsh_closed_issues,
+                                                         "dsh_open_issues":dsh_open_issues,
+                                                         "dsh_closed_rate":dsh_closed_rate,
+                                                         "dsh_contrib": dsh_contrib})
