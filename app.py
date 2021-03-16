969679dd030903cde8d79d4e8d12cd58a661df3a
retroam <retroam@yahoo.com>
2021-03-16 23:29:52 +0000

    reformated files

--- app.py
+++ app.py
@@ -10,22 +10,34 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 config = configparser.ConfigParser()
-config.read('.config')
+config.read(".config")
 access_token = config.get("GITHUB", "token")
 username = config.get("GITHUB", "username")
 auth = (username, access_token)
+
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("welcome.html", {"request": request})
 
+
 @app.get("/analysis/", response_class=HTMLResponse)
 async def get_repo(request: Request, repo: str):
     data = get_data(repo, auth)
-    dsh_open_issues, dsh_closed_issues, dsh_closed_rate,dsh_contrib = get_dashboard_stats(data)
-    return templates.TemplateResponse("gitviz.html", {"request": request,
-                                                         "repo":repo,
-                                                         "dsh_closed_issues":dsh_closed_issues,
-                                                         "dsh_open_issues":dsh_open_issues,
-                                                         "dsh_closed_rate":dsh_closed_rate,
-                                                         "dsh_contrib": dsh_contrib})
+    (
+        dsh_open_issues,
+        dsh_closed_issues,
+        dsh_closed_rate,
+        dsh_contrib,
+    ) = get_dashboard_stats(data)
+    return templates.TemplateResponse(
+        "gitviz.html",
+        {
+            "request": request,
+            "repo": repo,
+            "dsh_closed_issues": dsh_closed_issues,
+            "dsh_open_issues": dsh_open_issues,
+            "dsh_closed_rate": dsh_closed_rate,
+            "dsh_contrib": dsh_contrib,
+        },
+    )
