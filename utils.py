275ba95aac6bad6e28639293ffca4340bdd620da
Robert Amanfu <robertamanfu@Roberts-MacBook-Air.local>
2021-03-17 21:17:40 -0700

    changes to utils

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 
-def get_data(repo, auth, state="all"):
+def get_data(repo: str, auth: tuple, state="all") -> pd.DataFrame:
     """
     Function for downloading issues data from GitHub
     """
@@ -37,17 +37,19 @@ def get_data(repo, auth, state="all"):
     )
     return output
 
-def get_group(data, field, limit=None, sort=False):
-    data['month'] = pd.to_datetime(data.created_at).dt.strftime('%Y-%m')
+
+def get_group(data: pd.DataFrame, field: str, limit=None, sort=False) -> tuple:
+    data["month"] = pd.to_datetime(data.created_at).dt.strftime("%Y-%m")
     grp = data.groupby(field).id.count()
     if sort:
         grp = grp.sort_values(ascending=False)
     if limit:
         grp = grp.head(limit)
-  
-    return list(grp.values),list(grp.index)
 
-def get_dashboard_stats(df):
+    return list(grp.values), list(grp.index)
+
+
+def get_dashboard_stats(df: pd.DataFrame) -> tuple:
     count_stats = df.state.value_counts().to_dict()
     count_stats_norm = round(100 * df.state.value_counts(normalize=True)).to_dict()
 
